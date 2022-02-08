2ab6e6d8708939ca69d5cbe7b30a37eed243ded5
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-02-08 17:26:07 +0900

    Modify: dup2(fd, STDIN_FILENO)

--- srcs/command_utility2.c
+++ srcs/command_utility2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command_utility2.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seungcoh <seungcoh@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jeunjeon <jeunjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 14:14:58 by jeunjeon          #+#    #+#             */
-/*   Updated: 2022/02/08 16:09:42 by seungcoh         ###   ########.fr       */
+/*   Updated: 2022/02/08 17:25:16 by jeunjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void	exe_cmd(char *cmd_path, t_argv *argv, char **envp, t_bool sig_flag)
 	pid = fork();
 	if (pid > 0)
 	{
-		child = waitpid(pid, &stat_loc, WUNTRACED);
+		child = waitpid(pid, &stat_loc, 0x00000002); // WUNTRACED
 		//pipe_tmp는 입력, pipe_tmp2는 출력 파일로 고정
 		//이를 위한 복사
 		if (argv->was_pipe)
@@ -77,10 +77,6 @@ void	exe_cmd(char *cmd_path, t_argv *argv, char **envp, t_bool sig_flag)
 				write(fd, buf, ft_strlen(buf));
 			close(fd);
 			close(fd2);
-			fd2 = open("pipe_tmp2", O_RDONLY, 0644);
-			while (read(fd2, buf, sizeof(buf)))
-				write(1, buf, ft_strlen(buf));
-			close(fd2);
 			unlink("pipe_tmp2");
 		}
 		if (ft_wifexited(stat_loc) == TRUE)
@@ -92,13 +88,13 @@ void	exe_cmd(char *cmd_path, t_argv *argv, char **envp, t_bool sig_flag)
 		if (argv->was_pipe)// 명령어에 input이 없고 명령어 앞에 pipe가 있을 경우 이전 명령어가 만든 출력을 읽어와 argv에 넣어야함
 		{
 			fd = open("pipe_tmp", O_RDONLY, 0644);
-			dup2(fd, 0);
+			dup2(fd, STDIN_FILENO);
 			close(fd);
 		}
 		if (argv->is_pipe) //pipe 존재시 파일에 출력
 		{
 			fd2 = open("pipe_tmp2", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			dup2(fd2, 1);
+			dup2(fd2, STDOUT_FILENO);
 			close(fd2);
 		}
 		execve(cmd_path, argv->argv, envp);
