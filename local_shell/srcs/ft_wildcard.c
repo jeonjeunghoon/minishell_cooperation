5bbad2b7ac7d81b4aa95e15c1ccc124400686763
secul5972 <secul5972@naver.com>
2022-03-03 18:12:56 +0900

    echo $~*$~

--- local_shell/srcs/ft_wildcard.c
+++ local_shell/srcs/ft_wildcard.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_wildcard.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeunjeon <jeunjeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: seungcoh <seungcoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:01:40 by seungcoh          #+#    #+#             */
-/*   Updated: 2022/03/03 16:05:03 by jeunjeon         ###   ########.fr       */
+/*   Updated: 2022/03/03 18:11:27 by seungcoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,30 @@ void	init_get_wild_str(t_wild *wild, t_list **wild_token)
 	wild->start_idx = 0;
 	*wild_token = 0;
 	wild->flag = 0;
+	wild->str = 0;
 }
 
 void	refine_wild_token(t_wild *wild, t_list **wild_token, \
 char *token, t_mini *mini)
 {
+	char *tmp;
+
 	wild->tmp_token.token = (char *)malloc(sizeof(char) * \
 	(wild->i - wild->start_idx + 1));
 	ft_strlcpy(wild->tmp_token.token, token + wild->start_idx, \
 	wild->i - wild->start_idx + 1);
 	if (refine_str(mini, &wild->tmp_token, mini->envp) != ERROR)
+	{
 		ft_lstadd_back(wild_token, ft_lstnew(wild->tmp_token.token));
+		if (wild->str == 0)
+			wild->str = ft_strdup(wild->tmp_token.token);
+		else
+		{
+			tmp = ft_strjoin(wild->str, wild->tmp_token.token);
+			free(wild->str);
+			wild->str = tmp;
+		}
+	}
 }
 
 t_bool	jmp_to_wild(t_wild	*wild, char *token)
@@ -67,6 +80,8 @@ t_list	*get_wild_str(t_mini *mini, char *token)
 {
 	t_wild	wild;
 	t_list	*wild_token;
+	char	*tmp;
+	t_list	*ret;
 
 	init_get_wild_str(&wild, &wild_token);
 	while (token[wild.i])
@@ -78,6 +93,9 @@ t_list	*get_wild_str(t_mini *mini, char *token)
 				if (wild.i - wild.start_idx)
 					refine_wild_token(&wild, &wild_token, token, mini);
 				flag_control(&wild);
+				tmp = ft_strjoin(wild.str, "*");
+				free(wild.str);
+				wild.str = tmp;
 			}
 			wild.i++;
 		}
@@ -87,7 +105,12 @@ t_list	*get_wild_str(t_mini *mini, char *token)
 	if (wild.start_idx && wild.i - wild.start_idx)
 		wild.flag |= 2;
 	if (wild.flag & 4)
-		return (find_wild_str(wild_token, \
+	{
+		ret = (find_wild_str(wild_token, \
 		get_ls_list(mini->envp), wild.flag));
+		if (!ret)
+			ft_lstadd_back(&ret, ft_lstnew(wild.str));
+		return (ret);
+	}
 	return (0);
 }
